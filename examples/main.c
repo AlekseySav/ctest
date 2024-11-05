830b9a2a3e34680b0d785e086823fce7f7a5b1c9
schet <schet.v.komplax@gmail.com>
2024-11-05 22:09:32 +0300

    add examples

--- examples/main.c
+++ examples/main.c
@@ -4,16 +4,23 @@
 #include "ctest.h"
 
 int add(int a, int b) { return a + b; }
+
 int faulty_add(int a, int b) { return a - b; }
+
 int slow_add(int a, int b) {
     sleep(5);
     return a + b;
 }
 
 TEST(ok, add) {
-    ASSERT(add(2, 2) == 4);  // ok
+    ASSERT(add(2, 2) == 4);       // ok
+    ASSERT(slow_add(2, 2) == 4);  // ok
 }
 
-TEST(fail, aplusb) {
+TEST(fail, assert) {
     ASSERT(faulty_add(2, 2) == 4);  // fail
+}
+
+TEST(fail, timeout, TIMEOUT(1)) {
+    ASSERT(slow_add(2, 2) == 4);  // timeout
 }
